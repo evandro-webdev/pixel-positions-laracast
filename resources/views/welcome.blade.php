6b2b3e99564f2dc69dc48d05f286ec704fc268f2
evandro-webdev <evandromateus06@gmail.com>
2025-03-25 17:15:40 -0300

    feat: add job card component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-  Hello world
+  <section>
+    <h3 class="mb-6">Top jobs</h3>
+
+    <div class="grid lg:grid-cols-3 gap-8">
+      <x-job-card></x-job-card>
+      <x-job-card></x-job-card>
+      <x-job-card></x-job-card>
+    </div>
+  </section>
 </x-layout>

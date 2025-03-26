e7d87cd91b8858a0651aaf038e5f040bc8423943
evandro-webdev <evandromateus06@gmail.com>
2025-03-26 08:58:12 -0300

    update: add textSize prop to tag component

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,3 @@
-<a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">{{ $slot }}</a>
+@props(['textSize' => 'text-2xs'])
+
+<a class="bg-white/10 hover:bg-white/25 p-3 py-1 rounded-xl {{ $textSize }} font-bold cursor-pointer transition-colors duration-300">{{ $slot }}</a>

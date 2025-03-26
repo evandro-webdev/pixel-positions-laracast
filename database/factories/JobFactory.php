7216264403548d03607e839f76e0f416e1bb4e7e
evandro-webdev <evandromateus06@gmail.com>
2025-03-26 12:56:26 -0300

    chore: setup JobFactory

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class JobFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'employer_id' => Employer::factory(),
+      'title' => fake()->jobTitle(),
+      'salary' => fake()->randomElement(['$20,000 USD', '$30,000 USD', '$50,000 USD', '$80,000 USD']),
+      'location' => 'Remote',
+      'schedule' => fake()->randomElement(['Full Time', 'Part Time']),
+      'url' => fake()->url(),
+    ];
+  }
 }

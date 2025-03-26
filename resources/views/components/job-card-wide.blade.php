e075e46f4b28909108dcf0a90cc287f2d7156e11
evandro-webdev <evandromateus06@gmail.com>
2025-03-25 22:30:43 -0300

    feat: add job-card-wide component

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+  <div>
+    <img src="https://picsum.photos/seed/{{ rand(0, 10000) }}/100/100" alt="" class="rounded-xl">
+  </div>
+
+  <div class="flex-1 flex flex-col">
+    <a href="" class="text-sm text-gray-400">Laracasts</a>
+    
+    <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+    <p class="text-sm text-gray-400 mt-auto">Full Time - From $60.000</p>
+  </div>
+
+  <div>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+    <x-tag>Tag</x-tag>
+  </div>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;700;800&display=swap" rel="stylesheet">
   @vite(['resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-gro tesk">
+<body class="bg-black text-white font-hanken-grotesk">
 
   <div class="px-10">
     <nav class="flex justify-between items-center py-4 border-b border-white/10">

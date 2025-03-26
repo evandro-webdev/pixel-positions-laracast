a550cba369c2ca453fd8193fb4cc3ee449678524
evandro-webdev <evandromateus06@gmail.com>
2025-03-26 12:56:12 -0300

    chore: setup EmployerFactory

--- database/factories/EmployerFactory.php
+++ database/factories/EmployerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class EmployerFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'name' => fake()->name,
+      'logo' => fake()->imageUrl(),
+      'user_id' => User::factory(),
+    ];
+  }
 }

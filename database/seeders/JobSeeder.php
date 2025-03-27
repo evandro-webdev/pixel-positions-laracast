4952a7b0a223c6c867d4d8d4d8e9f4903d5550ac
evandro-webdev <evandromateus06@gmail.com>
2025-03-26 21:43:53 -0300

    chore: setup JobSeeder

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -2,16 +2,19 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class JobSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $tags = Tag::factory(3)->create();
+    Job::factory(20)->hasAttached($tags)->create();
+  }
 }

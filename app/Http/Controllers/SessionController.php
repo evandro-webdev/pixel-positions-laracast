949f016e5c0da1fb48eb785c3557919c119f51cb
evandro-webdev <evandromateus06@gmail.com>
2025-03-27 13:17:43 -0300

    feat: create/implement store method on SessionControler

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,17 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   */
-  public function index()
-  {
-    //
-  }
-
   /**
    * Show the form for creating a new resource.
    */
@@ -27,36 +22,22 @@ class SessionController extends Controller
    */
   public function store(Request $request)
   {
-    //
+    $attributes = $request->validate([
+      'email' => ['required', 'email', 'exists:users,email'],
+      'password' => ['required']
+    ]);
+
+    if (!Auth::attempt($attributes)) {
+      throw ValidationException::withMessages([
+        'email' => 'Sorry, those credentials do not match'
+      ]);
+    }
+
+    request()->session()->regenerate();
+
+    return redirect('/');
   }
 
-  /**
-   * Display the specified resource.
-   */
-  public function show(string $id)
-  {
-    //
-  }
-
-  /**
-   * Show the form for editing the specified resource.
-   */
-  public function edit(string $id)
-  {
-    //
-  }
-
-  /**
-   * Update the specified resource in storage.
-   */
-  public function update(Request $request, string $id)
-  {
-    //
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   */
   public function destroy(string $id)
   {
     //

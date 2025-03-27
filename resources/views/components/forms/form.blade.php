3af60ac4566d84013f4f336f7c88d4af2dfe5b3d
evandro-webdev <evandromateus06@gmail.com>
2025-03-27 15:16:52 -0300

    update: reanme method atribute do rest-method on form component

--- resources/views/components/forms/form.blade.php
+++ resources/views/components/forms/form.blade.php
@@ -1,7 +1,7 @@
 <form {{ $attributes(["class" => "max-w-2xl mx-auto space-y-6", "method" => "GET"]) }}>
     @if ($attributes->get('method', 'GET') !== 'GET')
         @csrf
-        @method($attributes->get('method'))
+        @method($attributes->get('rest-method'))
     @endif
 
     {{ $slot }}

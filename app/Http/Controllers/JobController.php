c712ec065fac8caf8068317c51958dbcc1ef87eb
evandro-webdev <evandromateus06@gmail.com>
2025-03-27 22:22:11 -0300

    feat: create/implement store job method on JobController

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Job;
 use App\Models\Tag;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
 
 class JobController extends Controller
 {
@@ -27,6 +31,30 @@ class JobController extends Controller
   public function create()
   {
     return view('jobs.create');
+  }
+
+  public function store(Request $request)
+  {
+    $attributes = $request->validate([
+      'title' => ['required'],
+      'salary' => ['required'],
+      'location' => ['required'],
+      'schedule' => ['required', Rule::in(['Part Time', 'Full Time'])],
+      'url' => ['required', 'active_url'],
+      'tags' => ['nullable']
+    ]);
+
+    $attributes['featured'] = $request->has('featured');
+
+    $job = Auth::user()->employer->jobs()->create(Arr::except($attributes, 'tags'));
+
+    if ($attributes['tags'] ?? false) {
+      foreach (explode(',', $attributes['tags']) as $tag) {
+        $job->tag($tag);
+      }
+    }
+
+    return redirect('/');
   }
 
   /**
